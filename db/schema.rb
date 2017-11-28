6311245e9f1c25f8e247353acc020b3d75090b5a
mibshara <bsharapc@gmail.com>
2017-11-28 12:54:27 -0600

    Another fix for a merging conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20171128100055) do
-=======
 ActiveRecord::Schema.define(version: 20171128090955) do
->>>>>>> parent of 2ee226e... Revert "added product relationships with product items"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -53,6 +49,7 @@ ActiveRecord::Schema.define(version: 20171128090955) do
   end
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   create_table "new_tables", force: :cascade do |t|
     t.string "name"
     t.string "password"
@@ -70,6 +67,8 @@ ActiveRecord::Schema.define(version: 20171128090955) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
 =======
+=======
+>>>>>>> parent of 2ee226e... Revert "added product relationships with product items"
   create_table "product_items", force: :cascade do |t|
     t.integer "product_id"
     t.integer "cart_id"
@@ -77,6 +76,9 @@ ActiveRecord::Schema.define(version: 20171128090955) do
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_product_items_on_cart_id"
     t.index ["product_id"], name: "index_product_items_on_product_id"
+<<<<<<< HEAD
+>>>>>>> parent of 2ee226e... Revert "added product relationships with product items"
+=======
 >>>>>>> parent of 2ee226e... Revert "added product relationships with product items"
   end
 
