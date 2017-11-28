c85426f3f66fce2b6ddf00419e2c163b29a0a408
mibshara <bsharapc@gmail.com>
2017-11-28 02:54:48 -0600

    added images for products, fixed some bugs and added pages controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
-//= require toastr
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'pages#index'
+  get 'shop' => 'pages#shop'
 
+  resources :product_items
+  resources :carts
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
