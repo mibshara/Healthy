5b5520a1ec2a1ad6c7839c9cbf56951711a2c989
mibshara <bsharapc@gmail.com>
2017-11-28 04:08:13 -0600

    Revert "tried to fix product items"
    This reverts commit feaf4faa3d9cead7acc0f9f508cb1fdff99838fa.

--- app/models/product_item.rb
+++ app/models/product_item.rb
@@ -1,5 +1,4 @@
 class ProductItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
-  belongs_to :order
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128100055) do
+ActiveRecord::Schema.define(version: 20171128091613) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -48,26 +48,15 @@ ActiveRecord::Schema.define(version: 20171128100055) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "new_tables", force: :cascade do |t|
-    t.string "name"
-    t.string "password"
+  create_table "product_items", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
+    t.index ["cart_id"], name: "index_product_items_on_cart_id"
+    t.index ["product_id"], name: "index_product_items_on_product_id"
   end
-
-  create_table "orders", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.text "address"
-    t.string "city"
-    t.string "province"
-    t.string "country"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-# Could not dump table "product_items" because of following StandardError
-#   Unknown type 'reference' for column 'order'
 
   create_table "products", force: :cascade do |t|
     t.string "title"
